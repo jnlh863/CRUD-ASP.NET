b44b36470db3f1d60b8341554e09814ebcfc06df
jnlh863 <116860102+jnlh863@users.noreply.github.com>
2023-10-29 22:33:09 -0600

    Web Api desarrollada en ASP.NET y desplegada en somee.com

---
+++ APIPruebas/Models/WebApiPruebaContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIPruebas.Models;
+
+public partial class WebApiPruebaContext : DbContext
+{
+    public WebApiPruebaContext()
+    {
+    }
+
+    public WebApiPruebaContext(DbContextOptions<WebApiPruebaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categorias { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.IdCategoria).HasName("PK__CATEGORI__A3C02A100B38F3AA");
+
+            entity.ToTable("CATEGORIAS");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("PK__PRODUCTO__098892100EB121C7");
+
+            entity.ToTable("PRODUCTO");
+
+            entity.Property(e => e.CodigoBarra)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Marca)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.oCategoria).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.IdCategoria)
+                .HasConstraintName("FK_IdCategoria");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
